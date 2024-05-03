e3e5989be7bdf2c05458c90c934cedcbb390ab6a
grahacr <courtney.graham@atlasschool.com>
2024-05-03 17:24:07 -0500

    lint spacing

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
-      console.log(task + task2);
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+    console.log(task + task2);
   }
+
+  return [task, task2];
+}
