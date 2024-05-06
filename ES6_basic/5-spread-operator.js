287634c90fc44629ce7bd6a876d7b7ba6dec86ca
grahacr <courtney.graham@atlasschool.com>
2024-05-06 17:09:25 -0500

    fix tab spacing for lint

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,5 +1,5 @@
 export default function concatArrays(array1, array2, string) {
-    const concatArray = [...array1, ...array2];
-    const resultArray = [...concatArray, ...string];
-    return resultArray;
+  const concatArray = [...array1, ...array2];
+  const resultArray = [...concatArray, ...string];
+  return resultArray;
 }
