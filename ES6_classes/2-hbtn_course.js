22035bd4633b03b95fa0bd09bc61d75b5a5ec2fd
grahacr <courtney.graham@atlasschool.com>
2024-05-07 14:29:09 -0500

    implement holbertoncourse class being sure to typecheck and use getters and setters

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,31 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    }
+    if (students instanceof Array) {
+      this._students = students;
+    }
+  };
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    return this._name = name;
+  }
+  get length() {
+    return this._length;
+  }
+  set length(length) {
+    return this._length = length;
+  }
+  get students() {
+    return this._students;
+  }
+  set students(students) {
+    return this._students = students;
+  }
+}
