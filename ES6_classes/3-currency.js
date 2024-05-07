2f46542d24b3809c59d72a5553d33cf77d39982a
grahacr <courtney.graham@atlasschool.com>
2024-05-07 16:41:40 -0500

    lint fixes

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -4,29 +4,34 @@ export default class Currency {
       throw new Error('Code must be a string');
     }
     if (typeof name !== 'string') {
-      throw new Error('Name must be a string')
+      throw new Error('Name must be a string');
     }
     this._name = name;
     this._code = code;
   }
+
   get code() {
     return this._code;
   }
+
   set code(code2) {
     if (typeof code2 !== 'string') {
       throw new Error('Code must be a string');
     }
     this._code = code2;
   }
+
   get name() {
     return this._name;
   }
+
   set name(name2) {
     if (typeof name2 !== 'string') {
       throw new Error('Name must be a string');
     }
     this._name = name2;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
