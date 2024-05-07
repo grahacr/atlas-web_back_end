aad2f8293eda3f877464969efe55ac41356b7d02
grahacr <courtney.graham@atlasschool.com>
2024-05-07 18:46:20 -0500

    fix for lint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -4,7 +4,7 @@ export default class Building {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
-    }
+  }
 
   get sqft() {
     return this._sqft;
