27adb8c191252fdd19af45545add998a4673c59d
grahacr <courtney.graham@atlasschool.com>
2024-05-07 18:43:05 -0500

    simplify and ensure instance cant even be created without method

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,12 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new Error('sqft must be a number');
-    } else {
-      this._sqft = sqft;
+    if (this.constructor !== Building && !(this.evacuationWarningMessage)) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
-  }
+    this._sqft = sqft;
+    }
 
   get sqft() {
     return this._sqft;
   }
 }
-
-if (typeof Building.evacuationWarningMessage !== 'function') {
-      throw new TypeError('Class extending Building must override evacuationWarningMessage');
-    }
