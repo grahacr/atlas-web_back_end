5441c5e989f4cb5736ddf6c62732abe375436399
grahacr <courtney.graham@atlasschool.com>
2024-05-07 18:59:03 -0500

    lint fixes

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,19 +1,19 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
- constructor(sqft, floors) {
-  super(sqft);
-  this._floors = floors;
- }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
- get sqft() {
-  return super.sqft;
- }
+  get sqft() {
+    return super.sqft;
+  }
 
- get floors() {
-  return this._floors;
- }
- evacuationWarningMessage() {
-  return `Evacuate slowly the ${this._floors} floors`
- }
+  get floors() {
+    return this._floors;
+  }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
