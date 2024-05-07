40db6990328099a67eee690d00f8939dfe2acda4
grahacr <courtney.graham@atlasschool.com>
2024-05-07 18:12:50 -0500

    create new pricing class utilizing another imported class. create class methods, one static.

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,44 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new Error('Amount must be number')
+    }
+    if (!(currency instanceof Currency)) {
+      throw new Error('Currency must be an instance of Currency')
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount2) {
+    if (typeof amount2 !== 'number') {
+      throw new Error('Amount must be number')
+    }
+    this._amount = amount2;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency2) {
+    if (!(currency instanceof Currency)) {
+      throw new Error('Currency must be an instance of Currency')
+    }
+    this._currency = currency2;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`
+  }
+
+  static convertPrice(amount, conversationRAte) {
+    return amount * conversationRate;
+  }
+}
