a44d24c9a204d3114d16e2f1475bd186feb985b0
grahacr <courtney.graham@atlasschool.com>
2024-05-06 21:11:13 -0500

    dangling comma

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const employeeInfo = {
-    [departmentName]: [...employees]
+    [departmentName]: [...employees],
   };
   return employeeInfo;
 }
