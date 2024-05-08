e89160fc2d4ad2d9c946891c6b164373f0e8693f
grahacr <courtney.graham@atlasschool.com>
2024-05-08 13:26:27 -0500

    take out variable and return array directly

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
 export default function getListStudents() {
-  const listStudents = [
+  return [
     { firstName: "Guillaume", id: 1, location: 'San Fransisco' },
     { firstName: "James", id: 2, location: 'Columbia' },
     { firstName: "Serena", id: 5, location: 'San Fransisco' },
   ];
-  return listStudents;
 }
