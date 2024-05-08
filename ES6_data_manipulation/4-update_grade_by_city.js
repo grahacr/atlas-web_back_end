4a34be5c8431187fdb6989b6df6f7f21ef4ec255
grahacr <courtney.graham@atlasschool.com>
2024-05-08 18:04:15 -0500

    lint fixes

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -4,9 +4,8 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
     const matchedGrade = newGrades.find((grade) => grade.studentId === student.id);
     if (matchedGrade) {
       return { ...student, grade: matchedGrade.grade };
-    } else {
-      return { ...student, grade: 'N/A' };
     }
+    return { ...student, grade: 'N/A' };
   });
   return gradeUpdate;
 }
