2299238e5dcbfb46e38c9003c9db530023433615
grahacr <courtney.graham@atlasschool.com>
2024-05-08 13:48:19 -0500

    san fran again

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,6 @@ export default function getListStudents() {
   return [
     { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
     { firstName: 'James', id: 2, location: 'Columbia' },
-    { firstName: 'Serena', id: 5, location: 'San Fransisco' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
   ];
 }
