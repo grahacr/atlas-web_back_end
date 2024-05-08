55a62135725150e1bc27ee581a1cc5ed6cdfcae3
grahacr <courtney.graham@atlasschool.com>
2024-05-08 13:11:52 -0500

    function creates and returns array with multiple properties

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,1 +1,8 @@
-function getListStudents()
+export default function getListStudents() {
+  const listStudents = [
+    { id: '1', firstName: 'Guillaume', location: 'San Fransisco'},
+    { id: '2', firstName: 'James', location: 'Columbia'},
+    { id: '5', firstName: 'Serena', location: 'San Fransisco'},
+  ];
+  return listStudents;
+}
