919daac695339f3114631986b07f096ae5974bb0
grahacr <courtney.graham@atlasschool.com>
2024-05-08 16:14:15 -0500

    lint fixes

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(students, city) {
-  const studentLocation = students.filter(student => student.location === city)
+  const studentLocation = students.filter((student) => student.location === city);
   return studentLocation;
 }
