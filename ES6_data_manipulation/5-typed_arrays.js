4f8c9796a9d5a105dbf8f4a10cf5972ddfc935c3
grahacr <courtney.graham@atlasschool.com>
2024-05-08 19:59:22 -0500

    fix return

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer);
   if (position >= length) {
     throw new Error('Position outside range');
   }
+  const view = new DataView(buffer);
   view.setInt8(position, value);
-  return buffer;
+  return view;
 }
