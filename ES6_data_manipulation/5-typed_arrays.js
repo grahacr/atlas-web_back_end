7c486b9c4de917f1bc8919af402fec3792ed8dc0
grahacr <courtney.graham@atlasschool.com>
2024-05-08 19:54:45 -0500

    utilize dataview instead and go back to returning buffer

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8array = new Int8Array(buffer);
+  const view = new DataView(buffer);
   if (position >= length) {
     throw new Error('Position outside range');
   }
-  int8array[position] = value;
-  return int8array;
+  view.setInt8(position, value);
+  return buffer;
 }
