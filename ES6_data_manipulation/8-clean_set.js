741ef656ac0e7d93e409d7d7dd7f81fccb8a958b
grahacr <courtney.graham@atlasschool.com>
2024-05-08 22:00:53 -0500

    lint fixes

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
-export default function cleanSet(set, startString){
+export default function cleanSet(set, startString) {
   let matches = '';
   if (typeof startString !== 'string' || startString.length === 0 || !startString) {
     return matches;
   }
   for (const value of set) {
     if (value.startsWith(startString)) {
-      let restOfString = value.substring(startString.length);
+      const restOfString = value.substring(startString.length);
       matches += (matches.length === 0 ? '' : '-') + restOfString;
     }
   }
