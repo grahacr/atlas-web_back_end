7528e4e9d2b8f098e3ae12690eaec6f39ee22586
grahacr <courtney.graham@atlasschool.com>
2024-05-08 23:59:03 -0500

    lint ugh

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,10 +2,10 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-  .then(([uploadPhotoResult, createUserResult]) => {
-    console.log(`${uploadPhotoResult.body} ${createUserResult.firstName} ${createUserResult.lastName}`);
-  })
-  .catch(() => {
-    console.log('Signup system offline');
-  });
+    .then(([uploadPhotoResult, createUserResult]) => {
+      console.log(`${uploadPhotoResult.body} ${createUserResult.firstName} ${createUserResult.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
