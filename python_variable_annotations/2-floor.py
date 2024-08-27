f82dca7a9e996caea66be0742ca8396dbf1e8e83
grahacr <courtney.graham@atlasschool.com>
2024-08-27 15:08:03 -0500

    using spaces instead of tabs? jesus

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -6,8 +6,8 @@ using type annotation, take a float as argument and return floor of float
 
 
 def floor(n: float) -> int:
-	'''
-	floor function takes float as argument: n
-	returns floor of that float: must be integer
-	'''
-	return math.floor(n)
+    '''
+    floor function takes float as argument: n
+    returns floor of that float: must be integer
+    '''
+    return math.floor(n)
