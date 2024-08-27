e4e4f79ce7bd8556795792ec0089704ea0f721c5
grahacr <courtney.graham@atlasschool.com>
2024-08-27 14:45:39 -0500

    pycodestyle fixes

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
-# function uses type annotations to ensure arguments are floats and return is float
+# function uses type annotations to ensure arguments are floats
+# and return is float
 def add(a: float, b: float) -> float:
-  return a + b
+    return a + b
