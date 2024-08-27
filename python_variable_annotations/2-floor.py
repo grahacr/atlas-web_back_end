b65df39e5f3d296eb33265fd26e09a58de79ff73
grahacr <courtney.graham@atlasschool.com>
2024-08-27 15:05:44 -0500

    floor function returns floor of float using type annotation

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import math
+'''
+using type annotation, take a float as argument and return floor of float
+'''
+
+
+def floor(n: float) -> int:
+	'''
+	floor function takes float as argument: n
+	returns floor of that float: must be integer
+	'''
+	return math.floor(n)
