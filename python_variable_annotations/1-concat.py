14ebcaa57e7a4363131db5352e3ee8371a69d4fb
grahacr <courtney.graham@atlasschool.com>
2024-08-27 14:55:12 -0500

    create simple type annotated function for concatenating two strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''this module shows a basic concatenation operation of two strings
+utilizing type annotations'''
+
+def concat(str1: str, str2: str) -> str:
+  '''
+  function concat for concatenating two strings:
+  str1 must be a string
+  str2 must be a string
+  function returns a combination of both strings
+  '''
+  return str1 + str2
