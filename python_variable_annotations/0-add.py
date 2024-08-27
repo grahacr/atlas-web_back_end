440df3c6c6aa20f0533c796a244fcb298b4ea186
grahacr <courtney.graham@atlasschool.com>
2024-08-27 14:50:10 -0500

    fix documentation style

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-
+'''this module shows the basic arithmetic operation of two floats
+utilizing type annotations'''
 def add(a: float, b: float) -> float:
-    # function uses type annotations to ensure arguments are floats
-    # and return is float
+    '''
+    add two floating-point numbers.
+    args: a (must be float) + b (must be float)
+    returns: float (Sum of two numbers passed in)
+    '''
     return a + b
