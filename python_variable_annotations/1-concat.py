79aa8cc8fe657ff69a4709efbeaa80741ef090d7
grahacr <courtney.graham@atlasschool.com>
2024-08-27 14:56:33 -0500

    pycodestyle spacing

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,11 +2,12 @@
 '''this module shows a basic concatenation operation of two strings
 utilizing type annotations'''
 
+
 def concat(str1: str, str2: str) -> str:
-  '''
-  function concat for concatenating two strings:
-  str1 must be a string
-  str2 must be a string
-  function returns a combination of both strings
-  '''
-  return str1 + str2
+    '''
+    function concat for concatenating two strings:
+    str1 must be a string
+    str2 must be a string
+    function returns a combination of both strings
+    '''
+    return str1 + str2
