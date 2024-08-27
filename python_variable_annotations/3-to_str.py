448af68f69cde7aee49c3eef2ca2159984fe7647
grahacr <courtney.graham@atlasschool.com>
2024-08-27 15:39:00 -0500

    create to_str function that takes float as argument and returns string representation of it

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+using type annotation, function takes float and returns
+string represntation of float passed in as argument
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    function to_str takes argument 'n' (float)
+    returns string representation of that float value
+    '''
+    return str(n)
