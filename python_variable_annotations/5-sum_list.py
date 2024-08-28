46d7ff915059e62e3b846b7f837548cee3a4a0e1
grahacr <courtney.graham@atlasschool.com>
2024-08-27 21:29:23 -0500

    documentation

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 '''
-using type annotation, 
+using type annotation, this module includes a function sum_list
+which will return the sum of all float types within a list
 '''
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    '''
+    function sum_list takes in one argument: input_list (list of float types)
+    return: sum of all float types within the list
+    '''
     return sum(input_list)
