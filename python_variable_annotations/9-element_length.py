58d8b83765c539626fe6f5329591c17dacaf1685
grahacr <courtney.graham@atlasschool.com>
2024-08-28 18:15:12 -0500

    change to more generalized approach with iterable sequence instead of list of strings, so it should match checker now, oops

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,7 +3,7 @@
 module adds type annotation to the element_length function
 utilizing List and Tuple from typing module
 '''
-from typing import List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 '''
 function element_length takes one argument:
 - lst (a list of string data types)
@@ -11,5 +11,5 @@ Returns:
 - a list of tuples, starting with string type, and then int type
 - The tuple contains the list element and then length of that element
 '''
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
