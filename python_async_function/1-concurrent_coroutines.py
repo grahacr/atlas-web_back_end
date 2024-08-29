f8cadbe12655988c48a294451c4af4ed053b7f91
grahacr <courtney.graham@atlasschool.com>
2024-08-28 22:07:35 -0500

    documentation and pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 '''
-
+module utilizes typing, random, and asyncio packages
+imports a function to generate a random delay
+function wait_n takes 2 integers as an argument to return a list of 
+delays, generated due to multiple coroutines running
+at the same time with async
 '''
 from typing import List
 import random
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    function wait_n takes two arguments:
+    - n (integer)
+    - max_delay (integer)
+    Returns:
+    - List of floats
+    Function generates list of delays (float values) as coroutines
+    are being executed, then returns that list
+    '''
     delays = []
     tasks = [wait_random(max_delay) for _ in range(n)]
 
