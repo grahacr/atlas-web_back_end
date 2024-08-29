7bfc843aecd48e6cdfb7f349444228fc67daa3dd
grahacr <courtney.graham@atlasschool.com>
2024-08-29 13:45:06 -0500

    import previous file and create measure_time function that measures the total execution time for wait_n and returns that total time in float form

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
-wait_n = __import__('1-concurrent_coroutines.py').wait_n
 '''
 '''
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    
+async def measure_time(n: int, max_delay: int) -> float:
+   start_time = time.time()
+   await wait_n(n, max_delay)
+   end_time = time.time()
+   elapsed_time = end_time - start_time
+   return float(elapsed_time)
