290220036217e941fd7ff958b9a5ab3cce466db3
grahacr <courtney.graham@atlasschool.com>
2024-08-29 14:01:11 -0500

    documentation

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
+'''
+module utilizes asyncio and imported function wait_random
+includes function task_wait_random which returns asyncio task
+'''
 
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''
+    task_wait_random takes one argument:
+    - max_delay (int)
+    Returns:
+    asyncio Task, which wraps around the wait_random coroutine
+    '''
     return asyncio.create_task(wait_random(max_delay))
