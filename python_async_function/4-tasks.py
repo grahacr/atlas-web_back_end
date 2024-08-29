79b4dd661f9193047da010521ca1462e07519b33
grahacr <courtney.graham@atlasschool.com>
2024-08-29 14:17:01 -0500

    utilize code from wait_n function but utilize task_Wait_random function instead when importing and calling outer function

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''
+module utilizes typing and asyncio packages
+imports a function to generate a random task delay
+function task_wait_n takes 2 integers as an argument to return a list of
+delays, generated due to multiple coroutines running
+at the same time with async
+'''
+from typing import List
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    function task_wait_n takes two arguments:
+    - n (integer)
+    - max_delay (integer)
+    Returns:
+    - List of floats
+    Function generates list of delays (float values) as coroutines
+    are being executed, then returns that list
+    '''
+    delays = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        index = 0
+        while index < len(delays) and delays[index] < delay:
+            index += 1
+        delays.insert(index, delay)
+
+    return delays
