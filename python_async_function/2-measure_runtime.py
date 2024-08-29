051af3d621df257120007c1faab64c08db3c2e29
grahacr <courtney.graham@atlasschool.com>
2024-08-29 13:49:04 -0500

    switch to regular function and call asyncio.run instead

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -6,9 +6,9 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
    start_time = time.time()
-   await wait_n(n, max_delay)
+   asyncio.run(wait_n(n, max_delay))
    end_time = time.time()
    elapsed_time = end_time - start_time
    return float(elapsed_time)
