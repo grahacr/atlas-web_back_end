ad15f381a47b24ed860b80fbc35e43329227e751
grahacr <courtney.graham@atlasschool.com>
2024-08-28 22:01:30 -0500

    fix file name in import

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@
 from typing import List
 import random
 import asyncio
-wait_random = __import__('0-basic-async-syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     delays = []
