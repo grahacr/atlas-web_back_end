eacdd9901992273dd8775dcb602352d01c470558
grahacr <courtney.graham@atlasschool.com>
2024-08-30 19:34:31 -0500

    documentation and fix return

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
-
+'''
+module includes an async_generator utilizing the following modules:
+asyncio, random, and AsyncGenerator from typing to accomplish the function
+of generating a random number after asynchronous delays
+'''
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[int, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
+    '''
+    async_generator takes no arguments.
+    it will asychronously wait 1 second, then
+    yield a random integer between 0 and 10
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
