9df8ade512b934f6ef6a6d2482552801bad1796d
grahacr <courtney.graham@atlasschool.com>
2024-09-03 15:42:50 -0500

    readme, add basecaching parent class, and create child class with get and put methods for cache system

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from base_caching import BaseCaching
+'''
+module imports BaseCaching and uses it as parent class for new BasicCache class
+which will implement a cache system with put and get methods.
+'''
+
+
+class BasicCache(BaseCaching):
+    '''BasicCache inherits from BaseCaching which is a straightforward parent class.
+    This class implements the put and get functions for cache system
+    '''
+    def __init__(self):
+        '''
+        constructor method calls parent constructor
+        '''
+        super().__init__()
+
+    def put(self, key, item):
+        '''
+        put method takes 3 arguments:
+        - self
+        - key
+        - item
+        if key and item are not empty, store item at given key
+        in the designated cache system
+        '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''
+        get method takes 2 arguments:
+        - self
+        - key
+        Returns: the value linked to the given key,
+        as long as key exists and is not None
+        '''
+        return self.cache_data.get(key, None)
---
+++ caching/README.md
@@ -0,0 +1,6 @@
+Caching!
+
+Finally learning about what caching is! This includes caching systems and cache eviction strategies, such as LIFO, FIFO, LFU, LRU, MRU.
+
+The purpose of a cache is to help applications run smoother or more quickly by accessing data that has already been stored so that potentially time-consuming/memory-consuming queries don't have to run from the source code every time, but can instead use data stored temporarily in the cache (often in-memory storage).
+
