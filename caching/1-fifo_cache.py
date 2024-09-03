3a6546242c7387b2ef347fa08a5bd2f044a5bedb
grahacr <courtney.graham@atlasschool.com>
2024-09-03 16:05:34 -0500

    documentation

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 '''
-
+this module imports parent class BaseCaching
+and adds a FIFO cache system, inserting data into a cache
+and deleting first item in if the max number of items is met
 '''
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
     '''
+    FIFOCache inherits from BaseCaching,
+    and implements FIFO eviction strategy to maintain cache dict
     '''
     def __init__(self):
         '''
@@ -16,6 +20,16 @@ class FIFOCache(BaseCaching):
         super().__init__()
 
     def put(self, key, item):
+        '''
+        put function takes 3 arguments:
+        - self
+        - key
+        - item
+        assigns item to given key, if key exists and is not None.
+        if MAX_ITEMS has been met for the cache dictionary,
+        FIFO method deletes first item in dictionary to make room
+        for new item, and prints Discard message if necessary
+        '''
         cache_data = self.cache_data
         if key is not None and item is not None:
             if len(cache_data) >= BaseCaching.MAX_ITEMS:
