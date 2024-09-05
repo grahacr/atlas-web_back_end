8450a133e553af0dc191072cd9ccca4279884bb1
grahacr <courtney.graham@atlasschool.com>
2024-09-05 14:34:19 -0500

    documentation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 '''
+module provides simple helper function for returning an
+index range to utilize for pagination
 '''
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    index_rangs funtion takes 2 arguments:
+    - page (int)
+    - page_size (int)
+    Return:
+    - Tuple (ints)
+    '''
     start_index = int((page - 1) * page_size)
     end_index = int(start_index + page_size)
     range = (start_index, end_index)
