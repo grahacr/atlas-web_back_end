aeca3c78d6692a7d06a69386388ae144f2d2ba18
grahacr <courtney.graham@atlasschool.com>
2024-09-11 21:17:59 -0500

    documentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''
-
+module includes function for obfuscating log message
+utilizing regex
 '''
 import re
 from typing import List
@@ -8,5 +9,15 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    pattern = '|'.join(f'(?<={field}=)[^;]*?(?={separator}|$)' for field in fields)
+    '''
+    filter_datum takes 4 arguments:
+    - fields (a list of strings)
+    - redaction (string) - to replace sensitive info
+    - message (string) - full string to replace PII
+    - separator (string) - separator to search for
+    Returns:
+    - String: obfuscated using redaction string
+    '''
+    pattern = ('|'.join(f'(?<={field}=)[^;]*?(?={separator}|$)'
+                        for field in fields))
     return re.sub(pattern, redaction, message)
