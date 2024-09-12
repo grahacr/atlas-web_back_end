5dbefa05b810f4574b1618cc36a01a6146467824
grahacr <courtney.graham@atlasschool.com>
2024-09-11 21:13:23 -0500

    change from greedy to lazy regex expression

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,5 +8,5 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    pattern = '|'.join(f'(?<={field}=)[^;]+(?={separator}|$)' for field in fields)
+    pattern = '|'.join(f'(?<={field}=)[^;]*?(?={separator}|$)' for field in fields)
     return re.sub(pattern, redaction, message)
