69c9d2e18677a66fe6a51d8d88322eb5bea4253a
grahacr <courtney.graham@atlasschool.com>
2024-09-13 22:01:29 -0500

    add propagation = false

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -72,6 +72,7 @@ def get_logger() -> logging.Logger:
     '''
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
+    logger.propagate = False
     stream_handler = logging.StreamHandler()
     formatter = RedactingFormatter(fields=PII_FIELDS)
     stream_handler.setFormatter(formatter)
