9e740b778ac0acb0a850d6b3453883aaffb36839
grahacr <courtney.graham@atlasschool.com>
2024-09-16 18:10:46 -0500

    not sure what i'm doing but trying to rename auth type variable

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,10 +14,13 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-auth = os.getenv('AUTH_TYPE')
-if auth:
+AUTH_TYPE = os.getenv('AUTH_TYPE')
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
