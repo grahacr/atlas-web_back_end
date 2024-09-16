3d11a4fc5e6873617078a2810b8a11308b0a642c
grahacr <courtney.graham@atlasschool.com>
2024-09-16 13:28:24 -0500

    documentation

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -16,6 +16,15 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
     return hashed
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''
+    is_valid function takes 2 args:
+    - hashed_password (bytes)
+    - password (string)
+    Returns: boolean on password validity
+    function encodes password to bytes and uses checkpw to 
+    validate it against hashed_password
+    '''
     password_bytes = password.encode()
     return bcrypt.checkpw(password_bytes, hashed_password)
