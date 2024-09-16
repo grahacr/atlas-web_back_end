17685c5b7fb8ce0eba9cbc4f289917ad83e0f0aa
grahacr <courtney.graham@atlasschool.com>
2024-09-16 16:04:00 -0500

    add another error handler and endpoint to check error handler for "forbidden"

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -31,3 +31,9 @@ def unauthorized():
     ''' testing unauthorized endpoint
     '''
     abort(401)
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    '''
+    '''
+    abort(403)
