a052e2370fb7a4f9a8862dc994c3b4d1dea5d21d
grahacr <courtney.graham@atlasschool.com>
2024-09-16 18:21:12 -0500

    document

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,6 +12,13 @@ class BasicAuth(Auth):
     BasicAuth class inherits from Auth
     '''
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        '''
+        function extracts basic authorization header
+        takes 2 args:
+        - self
+        - authorization_header (string)
+        return: string representing basic auth header
+        '''
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
