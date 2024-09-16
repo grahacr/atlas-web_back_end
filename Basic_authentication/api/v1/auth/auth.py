36d54b2eed5b66f0533a196b74918f0e95fae42e
grahacr <courtney.graham@atlasschool.com>
2024-09-16 17:39:40 -0500

    complete authorization_header function to read header, then add to app file an authorization instance and request filtering

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -40,7 +40,9 @@ class Auth():
         - request (default value = None)
         Return: string
         '''
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''
