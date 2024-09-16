340e77557c233deaffa8af678905265ebd34422a
grahacr <courtney.graham@atlasschool.com>
2024-09-16 16:45:28 -0500

    documentation

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -7,14 +7,35 @@ from typing import List, TypeVar
 
 class Auth():
     '''
+    Authentication class contains functions for
+    basic Authentication
     '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''
+        require_auth function takes 3 args:
+        - self
+        - path (string)
+        - excluded_paths (list of strings)
+        Return: boolean
+        '''
         return False
     
 
     def authorization_header(self, request=None) -> str:
+        '''
+        authorization_header takes 2 args:
+        - self
+        - request (default value = None)
+        Return: string
+        '''
         return None
     
     
     def current_user(self, request=None) -> TypeVar('User'):
+        '''
+        current user function takes 2 args:
+        - self
+        - request (default value = None)
+        Return: Type ('user')
+        '''
         return None
