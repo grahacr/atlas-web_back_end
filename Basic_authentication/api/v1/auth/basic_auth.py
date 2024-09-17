3563275fe8877a437191fde017f60b08b12bff40
grahacr <courtney.graham@atlasschool.com>
2024-09-16 21:22:34 -0500

    add try except statement around logic

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -79,10 +79,14 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None and not isinstance(user_pwd, str):
             return None
-        users = User.search({'email': user_email})
-        if not users:
+        
+        try:
+            users = User.search({'email': user_email})
+            if not users:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+
+        except Exception:
             return None
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
-        return None
