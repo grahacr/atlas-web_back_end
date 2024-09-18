2a37452f28db386a48fe660555d12e3d342c2fe7
grahacr <courtney.graham@atlasschool.com>
2024-09-17 20:07:19 -0500

    fix syntax issues in filter request method for correct error output

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -42,12 +42,12 @@ def filter_request():
     ]
     if not auth.require_auth(request.path, specific_paths):
         return
-    if (auth.authorization_header(request) and
+    if (auth.authorization_header(request) is None and
         auth.session_cookie(request) is None):
         abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
     request.current_user = auth.current_user(request)
+    if request.current_user is None:
+        abort(403)
 
 
 @app.errorhandler(404)
