a7727fa6ac69c0e621e35a96e480f2b291058a39
grahacr <courtney.graham@atlasschool.com>
2024-09-17 20:04:03 -0500

    exclude auth_session/login path along with other excluded paths in before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -37,11 +37,13 @@ def filter_request():
     specific_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if not auth.require_auth(request.path, specific_paths):
         return
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) and
+        auth.session_cookie(request) is None):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
