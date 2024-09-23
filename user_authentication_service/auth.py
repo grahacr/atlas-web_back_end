2189b916d920cea79225537cd85e9134c80fb3c6
grahacr <courtney.graham@atlasschool.com>
2024-09-22 23:07:30 -0500

    add method to register user in database after hasing the password and checking the user doesnt already exist

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """auth module
 """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +13,21 @@ def _hash_password(password: str) -> bytes:
     password_bytes = password.encode()
     hashed = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    
+    def register_user(self, email: str, password: str) -> User:
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError('User {email} already exists')
+        except:
+            hashed_password = _hash_password(password)
+            registered_user = self._db.add_user(email, hashed_password)
+            return registered_user
