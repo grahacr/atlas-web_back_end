d9b94c4584b58310bc22e5aa052103846a49795d
grahacr <courtney.graham@atlasschool.com>
2024-09-22 21:37:39 -0500

    implement method to find user by keyword arguments otherwise raising errors

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from typing import TypeVar
 from user import User, Base
 
@@ -43,3 +45,17 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        '''
+        find_user_by method takes arbritrary arguments
+        - kwargs
+        Returns: User from table by filtering
+        using kwargs keywords
+        '''
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        if TypeError:
+            raise InvalidRequestError
+        return user
