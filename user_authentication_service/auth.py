ff01c9fe6f0accfc9f1853105bbe9bfc894ed860
grahacr <courtney.graham@atlasschool.com>
2024-09-22 23:11:46 -0500

    add imports to fix no result found in except statement

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,6 +4,8 @@
 import bcrypt
 from db import DB
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 def _hash_password(password: str) -> bytes:
@@ -24,10 +26,17 @@ class Auth:
 
     
     def register_user(self, email: str, password: str) -> User:
+        '''
+        register_user method takes 3 args:
+        - self
+        - email (string)
+        - password (string)
+        Return: User (newly registered in db)
+        '''
         try:
             self._db.find_user_by(email=email)
-            raise ValueError('User {email} already exists')
-        except:
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
             hashed_password = _hash_password(password)
             registered_user = self._db.add_user(email, hashed_password)
             return registered_user
