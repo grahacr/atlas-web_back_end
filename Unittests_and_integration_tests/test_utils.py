a5dee9aae6aa28f7d3a9c4cfb60ea4cc7da5a6d4
grahacr <courtney.graham@atlasschool.com>
2024-09-24 13:36:13 -0500

    fix syntax of using keyerror

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -20,12 +20,13 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(result, expected)
 
     @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError)
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected):
-        result = access_nested_map(nested_map, path)
-        self.assertRaises(result, expected)
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == "__main__":
     unittest.main()
