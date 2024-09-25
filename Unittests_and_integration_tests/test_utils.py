951fe1e4d4f3cb3de6a69f4f07f01fa7eb2ffd21
grahacr <courtney.graham@atlasschool.com>
2024-09-25 09:24:42 -0500

    more documentation

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -17,6 +17,13 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        '''
+        class method takes 4 args:
+        -self
+        -nested_map
+        -path(specific path of nested map)
+        -expected (expected output)
+        '''
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
 
@@ -25,6 +32,12 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
+        '''
+        test nested map exception method. takes 3 args:
+        - self
+        - nested_map
+        - path
+        '''
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
@@ -41,6 +54,13 @@ class TestGetJson(unittest.TestCase):
     ])
     @patch('requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
+        '''
+        method to test get json using mock. takes 3 args:
+        -self
+        -test_url
+        -test_payload
+        -mock_get (patched get method)
+        '''
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
