036013cb1a5c365e56c6ecb1aa98539c59e5530d
grahacr <courtney.graham@atlasschool.com>
2024-09-25 23:11:04 -0500

    setting up test_has_license

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -79,6 +79,13 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public_repos_url.return_value)
         mock_public_repos_url.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+
+        
 
 if __name__ == "__main__":
     unittest.main()
