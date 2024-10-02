4593b2ae6a18e6afbdea7493c68f0c567e486a14
grahacr <courtney.graham@atlasschool.com>
2024-10-02 14:21:47 -0500

    forgot a comma?

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM ('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+    country ENUM ('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
     PRIMARY KEY (id)
 );
