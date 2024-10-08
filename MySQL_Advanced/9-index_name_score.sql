f392fe5191dbbe8fcc76e3ad27da383a2ffedfe9
grahacr <courtney.graham@atlasschool.com>
2024-10-08 14:47:38 -0500

    whoops, fix variable name

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -2,4 +2,4 @@
 -- using first letter of name and a score range
 -- enabling us to search for names that start with a certain letter and with certain score values
 
-CREATE INDEX idx_name_first ON names (name(1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
