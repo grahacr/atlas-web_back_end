06e86ae8a5c1ef7df83da78793f466dd509021b5
grahacr <courtney.graham@atlasschool.com>
2024-10-08 14:46:43 -0500

    create composite index that searches for first letter of name and score

---
+++ MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,5 @@
+-- SQL script creating index on "names" table
+-- using first letter of name and a score range
+-- enabling us to search for names that start with a certain letter and with certain score values
+
+CREATE INDEX idx_name_first ON names (name(1), score);
