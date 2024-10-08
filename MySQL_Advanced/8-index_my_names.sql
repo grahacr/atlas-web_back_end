29c0c17aeca87194d758135431b95ad3392b6471
grahacr <courtney.graham@atlasschool.com>
2024-10-08 14:26:54 -0500

    change syntax of column index

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- SQL script creating index on "names" table using first letter of name
 -- enabling us to search for names that start with a certain letter
 
-CREATE INDEX idx_name_first ON names (name, 1);
+CREATE INDEX idx_name_first ON names (name);
