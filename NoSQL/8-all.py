74a99d2dca2e9c36b12d2bd6a9613d730a52e874
grahacr <courtney.graham@atlasschool.com>
2024-10-09 22:25:25 -0500

    python script to list all docs in a collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+module to list all documents in a collection
+'''
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    '''
+    list_all function takes 1 arg:
+    - mongo_collection (collection to check)
+    Return:
+    Empty list if collection is none
+    otherwise, list of documents in collection
+    '''
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
