ac10910aabfd0332813e266632942aa41c1eb474
grahacr <courtney.graham@atlasschool.com>
2024-10-23 14:01:09 -0500

    fix if statement to help output

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -5,8 +5,10 @@ app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 });
 
-app.listen(7865, () => {
-    console.log('API available on localhost port 7865');
-});
+if (require.main === module) {
+    app.listen(7865, () => {
+        console.log('API available on localhost port 7865');
+    });
+}
 
 module.exports = app;
